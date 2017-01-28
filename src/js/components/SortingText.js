ff054d7091c532952ec82189e758fa9eca42b087
micklethepickle <michel_ma@hotmail.ca>
2017-01-28 15:30:15 -0500

    idk

--- src/js/components/SortingText.js
+++ src/js/components/SortingText.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default class SortingText extends React.Component {
 	render(){
+		const listItems = this.props.toSortArray.map((number) =>
+  			<li>{number}</li>)
 		return(
-			<ul>{this.props.toSortArray}</ul>
+			<ul class="no-bullets">{listItems}</ul>
 		);
 	}
 }
